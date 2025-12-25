d90d22dfbc3c58db0c9278395944360228116c07
olschlue <post@oschlueter.de>
2025-12-25 09:59:52 +0000

    Fix: UTF-8 Zeichenkodierung für alle Sprachen vereinheitlicht
    - Alle Sprachen verwenden jetzt UTF-8 statt ISO-8859-x
    - Behebt Problem mit fehlerhafter Umlaut-Darstellung (z.B. Schlüter -> SchlÃŒter)
    - Betrifft: Deutsch, Englisch, Polnisch, Kroatisch, Französisch, Italienisch, Spanisch, Niederländisch

--- include/language.inc.php
+++ include/language.inc.php
@@ -30,50 +30,42 @@ if(isset($_SESSION['Language']) && $_SESSION['Language'] == '1') {
 	require_once("language/german.php");
 	$_SESSION['Help'] = "help/de";
 	setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'de_DE.UTF-8');
-	$_SESSION['Charset'] = "utf-8";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } elseif(isset($_SESSION['Language']) && $_SESSION['Language']== '2') {
 	require_once("language/english.php");
 	$_SESSION['Help'] = "help/en";
 	setlocale(LC_ALL, 'en_EN@euro', 'en_EN', 'en_EN.UTF-8');
-	$_SESSION['Charset'] = "ISO-8859-15";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } elseif(isset($_SESSION['Language']) && $_SESSION['Language']== '3') {
 	require_once("language/polish.php");
 	$_SESSION['Help'] = "help/pl";
 	setlocale(LC_ALL, 'pl_PL@euro', 'pl_PL', 'pl_PL.UTF-8');
-	$_SESSION['Charset'] = "ISO-8859-2";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } elseif(isset($_SESSION['Language']) && $_SESSION['Language']== '4') {
 	require_once("language/croatian.php");
 	$_SESSION['Help'] = "help/hr";
 	setlocale(LC_ALL, 'hr_HR@euro', 'hr_HR', 'hr_HR.UTF-8');
-	$_SESSION['Charset'] = "ISO-8859-2";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } elseif(isset($_SESSION['Language']) && $_SESSION['Language']== '5') {
 	require_once("language/french.php");
 	$_SESSION['Help'] = "help/fr";
 	setlocale(LC_ALL, 'fr_FR@euro', 'fr_FR', 'fr_FR.UTF-8');
-	$_SESSION['Charset'] = "ISO-8859-15";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } else if(isset($_SESSION['Language']) && $_SESSION['Language']== '6') {
 	require_once("language/italian.php");
 	$_SESSION['Help'] = "help/it";
 	setlocale(LC_ALL, 'it_IT@euro', 'it_IT', 'it_IT.UTF-8');
-	$_SESSION['Charset'] = "ISO-8859-1";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } elseif(isset($_SESSION['Language']) && $_SESSION['Language']== '7') {
 	require_once("language/spanish.php");
 	$_SESSION['Help'] = "help/es";
 	setlocale(LC_ALL, 'es_ES@euro', 'es_ES');
-	$_SESSION['Charset'] = "ISO-8859-15";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } elseif(isset($_SESSION['Language']) && $_SESSION['Language']== '8') {
 	require_once("language/dutch.php");
 	$_SESSION['Help'] = "help/nl";
 	setlocale(LC_ALL, 'nl_NL@euro', 'nl_NL');
-	$_SESSION['Charset'] = "ISO-8859-15";
-	//$_SESSION['Charset'] = "UTF-8";
+	$_SESSION['Charset'] = "UTF-8";
 } else {
 	//	Chooose a language during logon
 	//
@@ -81,49 +73,49 @@ if(isset($_SESSION['Language']) && $_SESSION['Language'] == '1') {
 	//
 	//require_once("language/german.php");
 	//setlocale(LC_ALL, 'de_DE@euro', 'de_DE', 'de_DE.UTF-8');
-	//$_SESSION['Charset'] = "ISO-8859-15";
+	//$_SESSION['Charset'] = "UTF-8";
 	
 	//	English
 	//
 	require_once("language/english.php");
 	setlocale(LC_ALL, 'en_EN@euro', 'en_EN', 'en_EN.UTF-8');
-	$_SESSION['Charset'] = "ISO-8859-15";
+	$_SESSION['Charset'] = "UTF-8";
 	
 	//	Polish
 	//
 	//require_once("language/polish.php");
 	//setlocale(LC_ALL, 'pl_PL@euro', 'pl_PL', 'pl_PL.UTF-8');
-	//$_SESSION['Charset'] = "ISO-8859-2";
+	//$_SESSION['Charset'] = "UTF-8";
 	
 	//	Croatian
 	//
 	//require_once("language/croatian.php");
 	//setlocale(LC_ALL, 'hr_HR@euro', 'hr_HR', 'hr_HR.UTF-8');
-	//$_SESSION['Charset'] = "ISO-8859-2";
+	//$_SESSION['Charset'] = "UTF-8";
 	
 	//	French
 	//
 	//require_once("language/french.php");
 	//setlocale(LC_ALL, 'fr_FR@euro', 'fr_FR', 'fr_FR.UTF-8');
-	//$_SESSION['Charset'] = "ISO-8859-15";
+	//$_SESSION['Charset'] = "UTF-8";
 	
 	// Italian
 	//
 	//require_once("language/italian.php");
 	//setlocale(LC_ALL, 'it_IT@euro', 'it_IT', 'it_IT.UTF-8');
-	//$_SESSION['Charset'] = "ISO-8859-1";
+	//$_SESSION['Charset'] = "UTF-8";
 	
 	// Spanish
 	//
 	//require_once("language/spanish.php");
 	//setlocale(LC_ALL, 'es_ES@euro', 'es_ES', 'es_ES.UTF-8');
-	//$_SESSION['Charset'] = "ISO-8859-15";
+	//$_SESSION['Charset'] = "UTF-8";
 
 	// Dutch
 	//
 	//require_once("language/dutch.php");
 	//setlocale(LC_ALL, 'nl_NL@euro', 'nl_NL', 'nl_NL.UTF-8');
-	//$_SESSION['Charset'] = "ISO-8859-15";
+	//$_SESSION['Charset'] = "UTF-8";
 }
 
 ?>
